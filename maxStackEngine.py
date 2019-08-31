92234098c621edc9f1aa52d0abdbf3039978fd2d
jonathanpopham <jonathan.popham@gmail.com>
2019-08-31 17:14:06 -0400

    WIP adding features for minvalue, updating ui

--- maxStackEngine.py
+++ maxStackEngine.py
@@ -1,18 +1,18 @@
 
 class MaxStack:
 	def __init__(self):
+
 		self.stack = []
-		self.current_max = None
+		self.maxTrackStack = []
+		self.minTrackStack = []
 
-	def push(self, value):
-		if not self.current_max
-			self.current_max = value
-		else:
-				self.current_max = max(self.current_max, value)
-			self.stack.append((val, self.current_max))
 
-		def pop(self):
-			self.stack.pop()
+	def push(self, val):
+		self.stack.append(value)
 
-		def max(self):
-				return self.stack[len(self.stack)-1][1]
+
+	def pop(self):
+
+	def getMax()
+		return self.maxTrackStack[-1]
+		
